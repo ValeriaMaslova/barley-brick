0522f34dc99acd06238d25e421a06d7e2990225f
Epiriget <epiriget28@gmail.com>
2016-06-01 10:30:06 +0600

    Added movement, it works, at last

--- project/hollow.c
+++ project/hollow.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <conio.h>
 
 const int N = 4;
 
@@ -10,6 +11,27 @@ void swap(int *a, int *b)
 	*a = *b;
 	*b = buff;
 }
+
+void printField(int field[][N], int x, int y, int *score)
+{
+	int i, j;
+	system("cls");             //System func
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			if (field[i][j] != 0) {
+				printf(" [%3d] ", field[i][j]);
+			}
+			else {
+				printf(" [   ] ");
+			}
+		}
+		printf("\n");
+	}
+	*score = *score + 1;
+//	printf("\nXY: %d   %d", x, y);
+	printf("\nScore: %d", *score);
+}
+
 
 bool checkSolution(int field[][N])
 {
@@ -28,6 +50,7 @@ bool checkSolution(int field[][N])
 	}
 	return 1;
 }
+
 
 void initializeField(int field[][N])
 {
@@ -57,25 +80,72 @@ void shuffleField(int field[][N])
 		}
 	}
 
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			printf("%d ", field[i][j]);
-		}
-		printf("\n");
-	}
-
 	while (checkSolution(field) == 1) {
 		shuffleField(field);
 	}
 }
 
+void findEmpty(int field[][N], int *x, int *y)
+{
+	int i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			if (field[i][j] == 0) {
+				*y = i;
+				*x = j;
+			}
+		}
+	}
+}
+
+void moveBrick(int field[][N], unsigned char btn, int *x, int *y)
+{
+	switch (btn) {
+	case 'a':
+		if (*x < N - 1) {
+			swap(&field[*y][*x], &field[*y][*x + 1]);
+			*x = *x + 1;
+		}
+		break;
+	case 'd':
+		if (*x > 0) {
+			swap(&field[*y][*x], &field[*y][*x - 1]);
+			*x = *x - 1;
+		}
+		break;
+	case 'w':
+		if (*y < N - 1) {
+			swap(&field[*y][*x], &field[*y + 1][*x]);
+			*y = *y + 1;
+		}
+		break;
+	case 's':
+		if (*y > 0) {
+			swap(&field[*y][*x], &field[*y - 1][*x]);
+			*y = *y - 1;
+		}
+		break;
+	default:
+		printf("\n\nSosi");
+		break;
+	}
+}
 
 int main()
 {
 	srand(time(NULL));
-	int field[N][N];  //Main field
+	int field[N][N], emptyX, emptyY, score = 0;  //Main field
+	unsigned char btn;
+
 	initializeField(field);
 	shuffleField(field);
+	findEmpty(field, &emptyX, &emptyY);
+	printField(field, emptyX, emptyY, &score);
+	while (true) {
+		btn = getch();     //conio.h
+		moveBrick(field, btn, &emptyX, &emptyY);
+		printField(field, emptyX, emptyY, &score);
+	}
 	system("pause");
 	return 0;
 }
