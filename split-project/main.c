551930ace20f7cd68509214b2cda6234210f3016
Maxim <maxim-155@yandex.ru>
2016-06-03 15:01:39 +0600

    console getch

--- split-project/main.c
+++ split-project/main.c
@@ -13,6 +13,20 @@ int main()
 	int field[N][N], emptyX, emptyY, score = 0;  //Main field
 	unsigned char btn;
 	
+	struct termios stored_settings;
+    	struct termios new_settings;
+        tcgetattr(0,&stored_settings);
+        new_settings = stored_settings;
+        new_settings.c_lflag &= (~ICANON);
+        new_settings.c_cc[VTIME] = 0;
+        new_settings.c_cc[VMIN] = 1;
+        tcsetattr(0,TCSANOW,&new_settings);
+
+	initializeField(field);
+	shuffleField(field);
+	findEmpty(field, &emptyX, &emptyY);
+	printField(field, emptyX, emptyY, score);
+
 	while (1) {
 		btn = getchar(); 
 		moveBrick(field, btn, &emptyX, &emptyY, &score);
@@ -22,6 +36,8 @@ int main()
 			break;
 		}
 	}
+
+	tcsetattr(0,TCSANOW,&stored_settings);
 
 	return 0;
 }
